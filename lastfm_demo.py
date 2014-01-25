f394a3f194965b9a52741a6e9f200c1610b8d3c9
Aaron Cronin <cronin@include.cat>
2014-01-25 14:09:35 +0000

    Update lastfm_demo.py

--- lastfm_demo.py
+++ lastfm_demo.py
@@ -3,6 +3,13 @@ from flask import Flask, request, redirect
 import requests
 import json
 
+'''
+    This basic demo shows querying an artist page using the audioscrobbler API.
+    Loading root/<artist name> will generate a page that shows the description and image associated with the artist.
+    
+    For various reasons, the homepage demonstrates the API result for "Kanye West".
+'''
+
 KEY = '149dd5b09740a5d715f2831fe791c262'
 SECRET = '326c2adac87600bdead990fd7e8ff4a8'
 
