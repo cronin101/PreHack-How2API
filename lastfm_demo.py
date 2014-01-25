168c34eb70460c440fcbf325679be20ca57024be
Aaron Cronin <cronin@include.cat>
2014-01-25 12:52:56 +0000

    lastfm api demo

---
+++ lastfm_demo.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, redirect
+
+import requests
+import json
+
+KEY = '149dd5b09740a5d715f2831fe791c262'
+SECRET = '326c2adac87600bdead990fd7e8ff4a8'
+
+API_ROOT = 'http://ws.audioscrobbler.com/2.0/'
+
+app = Flask(__name__)
+
+@app.route('/')
+def root_page():
+    return redirect('http://127.0.0.1:8080/kanye%20west')
+
+@app.route('/<artist>')
+def artist_page(artist):
+    payload = {
+        'method' : 'artist.getinfo',
+        'artist' : artist,
+        'api_key' : KEY,
+        'format' : 'json'
+    }
+
+    r = requests.get(API_ROOT, params=payload)
+    artist_details = json.loads(r.text)
+
+    summary =  artist_details['artist']['bio']['summary']
+    image =  artist_details['artist']['image'][-1]['#text']
+
+    page = '<html><head><link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"></head>' + summary + '<hr><img src="' + image + '"></html'
+
+    return page
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=8080)
